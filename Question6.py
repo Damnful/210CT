f917e63c86de503603e502039ea2c62d73676711
Max R <damnful@live.co.uk>
2016-12-01 03:29:56 +0000

    Update and rename 6.py to Question6.py

--- Question6.py
+++ Question6.py
@@ -1,12 +1,16 @@
 def split_sentence(sentence):
     
-    reversedSentence = []
+    reversedSentence = [] 
+    # empty sentence to append to
     splitSentence = sentence.split(" ")
+    # splits the words up in the sentence
+    
     for word in reversed(splitSentence):
         reversedSentence.append(word)
+        # for every word in the original sentence, we append to the new sentence
 
     for x in range(0, len(reversedSentence)):
         print(reversedSentence[x], end=" ")
-
+        # this is just used to print the reversed sentence in a nice format
 
 split_sentence('This is awesome')
