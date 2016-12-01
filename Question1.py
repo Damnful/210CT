4bd13eadcff0f944b88aa2aeb72b1f8a363cb72b
Max R <damnful@live.co.uk>
2016-12-01 03:31:51 +0000

    Update and rename Q1.py to Question1.py

--- Question1.py
+++ Question1.py
@@ -5,8 +5,10 @@ def shuffle(numberlist):
 
     for x in range(0,10):
         index = random.randint(0,((len(numberlist) -1)))
+        # gets an index of a random number
         pointer = numberlist[x]
         numberlist[x] = numberlist[index]
+        # swaps the index of the current iteration with the random number
         numberlist[index] = pointer
 
     print(str(numberlist))
