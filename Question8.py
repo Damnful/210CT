72e625ce3528a26a9475627d67f7334dc17e700a
Max R <damnful@live.co.uk>
2016-12-02 06:16:14 +0000

    Update Question8.py
    added pseudocode

--- Question8.py
+++ Question8.py
@@ -1,3 +1,13 @@
+'''
+REMOVE_VOWEL(string, x←0)
+	vowels ← a,e,i,o,u
+	IF x < 0 or if x > 4
+		RETURN string
+	ELSE
+		x ← x + 1
+		REMOVE_VOWEL(replace(string[x]) with nothing)
+'''
+
 def remove_Vowel(string, x=0):
     vowels = ("i", "e", "o", "u", "a")
     if x < 0 or x > 4:
